a7ed9b009333d2203a60736f9e289be289caa00b
YusufArdaT <yusufarda445@gmail.com>
2026-05-06 12:20:31 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 oyuncular= [
-    "Murat Akçakoç",
+    "Murat Akçakoç ©️",
     "Enes Altundaş",
     "Servet Karabat",
     "Servet Tekdemir",
